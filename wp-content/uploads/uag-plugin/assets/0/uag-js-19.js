01b0b8cc6fdb00710c05190f41e7bbe1004f5b25
Alleau Jérémy <66945318+jerem45@users.noreply.github.com>
2024-02-15 21:03:58 +0100

    first commit

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-19.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"8f5df558","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"jerem.dev45@gmail.com","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Soumission du formulaire","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Veuillez remplir le captcha ci-dessus.","confirmationUrl":""}, '.uagb-block-8f5df558', 19 );
+});
+ });
